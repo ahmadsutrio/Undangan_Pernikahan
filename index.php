9aaa7977e602e518793a0e9ed488322c2cfd4911
ahmadsutrio <12050113307@students.uin-suska.ac.id>
2024-05-24 22:12:38 +0700

    memperbaiki classname

--- index.php
+++ index.php
@@ -11,15 +11,15 @@
     <meta property="og:url" content="https://tes.undangandigitalku.biz.id/">
     <link rel="shortcut icon" href="/public/img/thumnail-undangan.webp" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link rel="stylesheet" href="public/css/style.css?v=1.6 ">
+    <link rel="stylesheet" href="public/css/style.css?v=1.7 ">
 </head>
 
 <body>
-    <div class="flex relative overflow-hidden top-0 left-0 right-0 bottom-0 ">
+    <div class="relative top-0 bottom-0 left-0 right-0 flex overflow-hidden ">
         <div class="lg:fixed lg:left-0 lg:w-[calc(100%_-_400px)] hidden lg:flex justify-center items-center ">
             <img src="/public/img/background.webp" alt="" class="w-full h-screen">
-            <div class="absolute top-0 left-0 right-0 bottom-0">
-                <div class="flex justify-center items-center">
+            <div class="absolute top-0 bottom-0 left-0 right-0">
+                <div class="flex items-center justify-center">
                     <img src="/public/img/iconic.webp" alt="" class="h-screen">
                 </div>
             </div>
@@ -27,35 +27,21 @@
 
 
         <div class="w-[400px] min-w-[360px] lg:fixed lg:right-0 mx-auto h-screen overflow-y-auto flex justify-center border overflow-x-hidden  bottom-0 ">
-            <section class="w-full  ">
+            <section class="w-full ">
                 <div class="" id="welcome">
                     <?php include('components/welcome.php'); ?>
                 </div>
-                <div class="hidden relative" id="tampilan">
-                    <div class="" data-aos="fade-up">
+                <div class="relative hidden" id="tampilan">
                         <?php include('components/timeline.php'); ?>
-                    </div>
-                    <div class="" data-aos="fade-up">
                         <?php include('components/pembukaan.php'); ?>
-                    </div>
-                    <div class="" data-aos="fade-up">
                         <?php include('components/savedate.php'); ?>
-                    </div>
-                    <div class="" data-aos="fade-up">
                         <?php include('components/pesan.php'); ?>
-                    </div>
-                    <div class="" data-aos="fade-up">
                         <?php include('components/kotakPesan.php'); ?>
-                    </div>
-                    <div class="" data-aos="fade-up">
                         <?php include('components/gift.php'); ?>
-                    </div>
-                    <div class="" data-aos="fade-up">
                         <?php include('components/footer.php'); ?>
-                    </div>
 
                     <div class="size-[59px] rounded-full right-8 z-[100]  bg-timeline flex justify-center items-center fixed bottom-10 animate-pulse border-timeline border-4">
-                        <div class="bg-white size-8 rounded-full  flex justify-center items-center cursor-pointer" onclick="playPause()">
+                        <div class="flex items-center justify-center bg-white rounded-full cursor-pointer size-8" onclick="playPause()">
                             <img src="/public/icons/pause.svg" alt="" srcset="" class="show" id="pause-music">
                             <img src="/public/icons/play.svg" alt="" srcset="" class="hidden" id="play-music">
                         </div>
