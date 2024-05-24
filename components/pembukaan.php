fbc7a278ae72d97e2ce23a7fde9f05dd94f64f17
ahmadsutrio <12050113307@students.uin-suska.ac.id>
2024-05-24 08:23:56 +0700

    update css and tampilan

--- components/pembukaan.php
+++ components/pembukaan.php
@@ -1,5 +1,5 @@
-   <div class=" text-center px-5 flex justify-center flex-col">
-       <img src="../public/img/basmallah.png" alt="" class="mx-auto">
+   <div class=" text-center px-5 flex justify-center h-screen flex-col">
+       <img src="../public/img/basmallah.webp" alt="" class="mx-auto mt-10">
        <p class="text-center mt-10">Assalamu’alaikum Wr. Wb.Tanpa mengurangi rasa hormat, kami mengundang Bapak/Ibu/Saudara/i serta kerabat sekalian untuk menghadiri acara pernikahan kami</p>
        <h4 class="mt-10 text-primary font-great-vebes text-[2.5rem]">
            Saputri, S.Pd
