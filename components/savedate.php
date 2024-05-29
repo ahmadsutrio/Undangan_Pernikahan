836dba7af34d2f4b99ec1568dbd9802c01cb2a6f
ahmadsutrio <12050113307@students.uin-suska.ac.id>
2024-05-29 17:55:42 +0700

    update versi css and revisi tampilan

--- components/savedate.php
+++ components/savedate.php
@@ -1,28 +1,28 @@
-<div class="relative h-[73rem] my-10 w-full " >
+<div class="relative h-[73rem] my-10 w-full ">
     <div class="absolute flex left-0 right-0 top-0 justify-center ">
-        <img loading="lazy" src="../public/img/batu-atas-664af056163d1.webp" alt="" class="">
+        <img src="../public/img/batu-atas-664af056163d1.webp" alt="" class="">
     </div>
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-20-664af0617fe96.webp" alt="" class="left-0 top-0 absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-1-664af0584697f.webp" alt="" class="right-0 top-0 absolute z-[1] ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-2-664af04e91770.webp" alt="" class="right-0 top-24 absolute z-10 ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-3-664af05923251.webp" alt="" class="right-0 top-44 absolute z-[1] ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-4-664af05d3f549.webp" alt="" class="right-0 top-52 absolute z-[1] ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-5-664af04f41546.webp" alt="" class="right-0 top-[22rem] absolute z-10 ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-6-664af04f6ca3b.webp" alt="" class="right-0 top-[22.7rem]  absolute z-[1] ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-7-664af05d3f6e6.webp" alt="" class="right-0 top-[30.7rem] absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-8-664af051c0387.webp" alt="" class="right-0 top-[38.7rem] absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-9-664af051c038b.webp" alt="" class="right-0 top-[44.7rem] z-10 absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-10-664af05e013a2.webp" alt="" class="right-0 top-[50.7rem] absolute ">
-    <img loading="lazy" src="../public/img/pasir-daun-bawah.webp" alt="" class="right-0 left-0  -bottom-[3rem] w-full absolute z-10 ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-12-664af05ed94ab.webp" alt="" class=" left-0  top-[46rem] z-[1] absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-12-1-664af05298c50.webp" alt="" class=" left-0  top-[41.5rem] z-10 absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-13-664af05ed94ae.webp" alt="" class=" left-0  top-[35rem] z-[1] absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-14-664af0536675d.webp" alt="" class=" left-0  top-[33rem] z-10 absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-15-664af060aa526.webp" alt="" class=" left-0  top-[25rem] z-[2] absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-16-664af060af9a1.webp" alt="" class=" left-0  top-[23rem] z-[1] absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-17-664af0544b7ff.webp" alt="" class=" left-0  top-[18rem] z-10 absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-18-664af0615c611.webp" alt="" class=" left-0  top-[10rem] z-[1] absolute ">
-    <img loading="lazy" src="../public/img/daun-tepi-save-date-19-664af05453e25.webp" alt="" class=" left-0  top-[4rem] z-10 absolute ">
+    <img src="../public/img/daun-tepi-save-date-20-664af0617fe96.webp" alt="" class="left-0 top-0 absolute ">
+    <img src="../public/img/daun-tepi-save-date-1-664af0584697f.webp" alt="" class="right-0 top-0 absolute z-[1] ">
+    <img src="../public/img/daun-tepi-save-date-2-664af04e91770.webp" alt="" class="right-0 top-24 absolute z-10 ">
+    <img src="../public/img/daun-tepi-save-date-3-664af05923251.webp" alt="" class="right-0 top-44 absolute z-[1] ">
+    <img src="../public/img/daun-tepi-save-date-4-664af05d3f549.webp" alt="" class="right-0 top-52 absolute z-[1] ">
+    <img src="../public/img/daun-tepi-save-date-5-664af04f41546.webp" alt="" class="right-0 top-[22rem] absolute z-10 ">
+    <img src="../public/img/daun-tepi-save-date-6-664af04f6ca3b.webp" alt="" class="right-0 top-[22.7rem]  absolute z-[1] ">
+    <img src="../public/img/daun-tepi-save-date-7-664af05d3f6e6.webp" alt="" class="right-0 top-[30.7rem] absolute ">
+    <img src="../public/img/daun-tepi-save-date-8-664af051c0387.webp" alt="" class="right-0 top-[38.7rem] absolute ">
+    <img src="../public/img/daun-tepi-save-date-9-664af051c038b.webp" alt="" class="right-0 top-[44.7rem] z-10 absolute ">
+    <img src="../public/img/daun-tepi-save-date-10-664af05e013a2.webp" alt="" class="right-0 top-[50.7rem] absolute ">
+    <img src="../public/img/pasir-daun-bawah.webp" alt="" class="right-0 left-0  -bottom-[3rem] w-full absolute z-10 ">
+    <img src="../public/img/daun-tepi-save-date-12-664af05ed94ab.webp" alt="" class=" left-0  top-[46rem] z-[1] absolute ">
+    <img src="../public/img/daun-tepi-save-date-12-1-664af05298c50.webp" alt="" class=" left-0  top-[41.5rem] z-10 absolute ">
+    <img src="../public/img/daun-tepi-save-date-13-664af05ed94ae.webp" alt="" class=" left-0  top-[35rem] z-[1] absolute ">
+    <img src="../public/img/daun-tepi-save-date-14-664af0536675d.webp" alt="" class=" left-0  top-[33rem] z-10 absolute ">
+    <img src="../public/img/daun-tepi-save-date-15-664af060aa526.webp" alt="" class=" left-0  top-[25rem] z-[2] absolute ">
+    <img src="../public/img/daun-tepi-save-date-16-664af060af9a1.webp" alt="" class=" left-0  top-[23rem] z-[1] absolute ">
+    <img src="../public/img/daun-tepi-save-date-17-664af0544b7ff.webp" alt="" class=" left-0  top-[18rem] z-10 absolute ">
+    <img src="../public/img/daun-tepi-save-date-18-664af0615c611.webp" alt="" class=" left-0  top-[10rem] z-[1] absolute ">
+    <img src="../public/img/daun-tepi-save-date-19-664af05453e25.webp" alt="" class=" left-0  top-[4rem] z-10 absolute ">
 
 
 
@@ -54,14 +54,22 @@
             </button>
 
             <div class="px-10 bg-slate-800/70 hidden backdrop-blur-lg z-50 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center flex-col" id="alamat">
-                <img loading="lazy" src="../public/img/qr_code.webp" alt="" class="size-48 block border-4 border-primary">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3989.818136149183!2d102.135326!3d0.01741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMMKwMDEnMDIuNyJOIDEwMsKwMDgnMDcuMiJF!5e0!3m2!1sen!2sid!4v1716432716176!5m2!1sen!2sid" width="250" height="150" style="border:0;" allowfullscreen="no" loading="lazy" class="mt-5 block  border-4 border-primary" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <h5 class="text-[2.6rem] font-great-vebes dark:text-d-heading">Acara Pesta</h5>
+                <img loading="lazy" src="../public/img/qr_code.webp" alt="" class="size-40 block border-4 border-primary">
+                <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3989.818136149183!2d102.135326!3d0.01741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMMKwMDEnMDIuNyJOIDEwMsKwMDgnMDcuMiJF!5e0!3m2!1sen!2sid!4v1716432716176!5m2!1sen!2sid"  style="border:0;" allowfullscreen="no" loading="lazy" class="mt-3 block  border-4 w-24 h-20 border-primary" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
 
-                <a href="https://maps.app.goo.gl/3zJC6GxGygu8hGGY9" target="_blank" class=" mt-5 mx-auto mb-12 text-white bg-timeline flex items-center justify-center rounded-lg gap-2 px-5 py-2 text-sm" onclick="lihatLokasi()">
+                <a href="https://maps.app.goo.gl/3zJC6GxGygu8hGGY9" target="_blank" class=" mt-5 mx-auto mb-4 text-white bg-timeline flex items-center justify-center rounded-lg gap-2 px-5 py-2 text-sm" onclick="lihatLokasi()">
+                    <img loading="lazy" src="../public/icons/bx_map.svg" alt="" class=""> Pergi Ke Lokasi
+                </a>
+                <h5 class="text-[2.6rem] font-great-vebes dark:text-d-heading mt-3">Ngunduh Mantu</h5>
+                <img loading="lazy" src="../public/img/qr_code.webp" alt="" class="size-40 block border-4 border-primary">
+                <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3989.818136149183!2d102.135326!3d0.01741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMMKwMDEnMDIuNyJOIDEwMsKwMDgnMDcuMiJF!5e0!3m2!1sen!2sid!4v1716432716176!5m2!1sen!2sid"  style="border:0;" allowfullscreen="no" loading="lazy" class="mt-3 block  border-4 w-24 h-20 border-primary" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+
+                <a href="https://www.google.com/maps?q=0.831571,102.073119&entry=gps&g_ep=CAESCjExLjExMS4yMDEYACDXggMqLSw5NDIyMTEzNyw5NDIxMjQ5Niw5NDIxMjY2Niw5NDIxODY1Myw0NzA4NDM5M0ICSUQ%3D&shorturl=1" target="_blank" class=" mt-5 mx-auto mb-10 text-white bg-timeline flex items-center justify-center rounded-lg gap-2 px-5 py-2 text-sm" onclick="lihatLokasi()">
                     <img loading="lazy" src="../public/icons/bx_map.svg" alt="" class=""> Pergi Ke Lokasi
                 </a>
 
-                <button class=" mt-5 mx-auto mb-12 text-white bg-timeline flex items-center justify-center rounded-lg gap-2 px-5 py-2 text-sm" onclick="lihatLokasi()"> Tutup
+                <button class=" mx-auto text-white bg-timeline flex items-center justify-center rounded-lg gap-2 px-5 py-2 text-sm" onclick="lihatLokasi()"> Tutup
                 </button>
             </div>
         </div>
